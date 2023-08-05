10b6c6a2f13999d45955b4e81c415652763459d7
Hasib Hossain <boraborhasib@gmail.com>
2023-08-06 00:01:42 +0600

    Router added

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,5 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../layout/MainLayout";
+import Home from "../pages/Home/Home";
 
 export const router = createBrowserRouter([
-    
+    {
+        path: '/',
+        element: <MainLayout />,
+        children: [
+            { path: '/', element: <Home /> },
+        ]
+    }
 ])
