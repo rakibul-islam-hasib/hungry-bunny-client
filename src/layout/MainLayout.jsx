582fd0518a9e6aa9276d1800f20a4aeb14f726fa
Anika <anika.bd1994@gmail.com>
2023-08-06 18:31:47 +0600

    footer part added

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import NavBar from '../components/header/NavBar';
+import Footer from '../components/footer/Footer';
 
 const MainLayout = () => {
     return (
         <main>
             <NavBar />
             <Outlet />
+            <Footer/>
         </main>
     );
 };
