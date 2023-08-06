a70ff1991f24940fa12d87b5cee8eaefa89c5104
Hasib Hossain <boraborhasib@gmail.com>
2023-08-06 09:26:11 +0600

    Logo changed

--- src/components/header/NavBar.jsx
+++ src/components/header/NavBar.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const NavBar = () => {
     return (
         <div>
-            <h1>Navbar</h1>
+            <h1 className='text-4xl text-center'>Navbar</h1>
         </div>
     );
 };
