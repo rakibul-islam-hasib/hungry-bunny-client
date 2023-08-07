4d21a544986b9707f7caec7104c7e5e54a013391
Hasib Hossain <boraborhasib@gmail.com>
2023-08-07 17:13:39 +0600

    Preloader added

---
+++ src/components/loader/Loader.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Loader.css';
+
+const Loader = () => {
+    return (
+        <div className='h-screen flex justify-center items-center'>
+            <div className="loader"></div>
+        </div>
+    );
+};
+
+export default Loader;
