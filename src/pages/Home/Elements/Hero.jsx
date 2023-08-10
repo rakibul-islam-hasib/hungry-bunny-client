1edfa8d063944f5423be559d55e16ef07629fc2d
Project_3 <anik2115@gmail.com>
2023-08-10 14:51:19 +0600

    card added

--- src/pages/Home/Elements/Hero.jsx
+++ src/pages/Home/Elements/Hero.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
 const Hero = () => {
-    return (
-        <div className='w-full ' >
-            <h1>This is Hero</h1>
-        </div>
-    );
+  return (
+    <div className="w-full ">
+      <h1>This is Hero section</h1>
+    </div>
+  );
 };
 
 export default Hero;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,16 @@
-import React from 'react';
-import Hero from './Elements/Hero';
-import CheckoutBar from '../../components/cart/CheckoutBar';
+import React from "react";
+import Hero from "./Elements/Hero";
+import CheckoutBar from "../../components/cart/CheckoutBar";
+import Card from "../../components/Card/Card";
 
 const Home = () => {
-    return (
-        <section>
-            <Hero />
-            <CheckoutBar />
-        </section>
-    );
+  return (
+    <section>
+      <Hero />
+      <Card></Card>
+      <CheckoutBar />
+    </section>
+  );
 };
 
 export default Home;
