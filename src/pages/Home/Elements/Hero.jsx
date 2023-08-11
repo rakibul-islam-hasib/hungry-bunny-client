2a89a5b114b535fb5dcab330b0be6f1f57a27134
Hasib Hossain <boraborhasib@gmail.com>
2023-08-11 19:04:52 +0600

    Updated

--- src/pages/Home/Elements/Hero.jsx
+++ src/pages/Home/Elements/Hero.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './css/Hero.css';
+import playIcon from '../../../assets/icons/play.svg';
+
 
 const Hero = () => {
   return (
     <section className='h-screen'>
       <div className="grid grid-cols-2 w-[90%] mx-auto">
-        <div className="">
+        <div className="flex flex-col mt-14 justify-center items-start h-full space-y-4">
           <div className="flex">
             <h1 className='text-8xl text-orange-500 font-bold'>Fast</h1>
             <div className="">
@@ -14,9 +16,16 @@ const Hero = () => {
             </div>
           </div>
           <p className='text-gray-500'>Imagine your hunger being met at warp speed. Our &quot;Supper First Delivery&quot; isn&apos;t just about delivering food; it&apos;s about delivering delight.</p>
-          <button className='hero-btn'>
-            <span className='text-white'>Order Now</span>
-          </button>
+
+          <div className="flex gap-3">
+            <button className=' px-7 py-4 text-xl font-bold text-white rounded-full bg-orange-500 '>Order Now</button>
+            <button className='flex justify-center items-center text-xl font-bold'>
+              <img src={playIcon} className='h-16 w-16' alt="" />
+              <span>Watch Video</span>
+            </button>
+          </div>
+
+
 
         </div>
         <div className="">
