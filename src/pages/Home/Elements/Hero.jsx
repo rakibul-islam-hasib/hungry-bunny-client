5f93cac1eee3d40d02f44231255dad89b9f370f3
Hasib Hossain <boraborhasib@gmail.com>
2023-08-11 18:16:19 +0600

    Home blog section added

--- src/pages/Home/Elements/Hero.jsx
+++ src/pages/Home/Elements/Hero.jsx
@@ -1,11 +1,38 @@
 import React from 'react';
+import './css/Hero.css';
 
 const Hero = () => {
   return (
-    <section>
+    <section className='h-screen'>
       <div className="grid grid-cols-2 w-[90%] mx-auto">
         <div className="">
-          <h1 className="">Welcome to the home page</h1>
+          <div className="flex">
+            <h1 className='text-8xl text-orange-500 font-bold'>Fast</h1>
+            <div className="">
+              <h1 className='text-4xl font-bold'>Food</h1>
+              <h1 className='text-4xl font-bold'>Delivery</h1>
+            </div>
+          </div>
+          <p className='text-gray-500'>Imagine your hunger being met at warp speed. Our &quot;Supper First Delivery&quot; isn&apos;t just about delivering food; it&apos;s about delivering delight.</p>
+          <button className='hero-btn'>
+            <span className="span-mother">
+              <span>B</span>
+              <span>u</span>
+              <span>t</span>
+              <span>t</span>
+              <span>o</span>
+              <span>n</span>
+            </span>
+            <span className="span-mother2">
+              <span>B</span>
+              <span>u</span>
+              <span>t</span>
+              <span>t</span>
+              <span>o</span>
+              <span>n</span>
+            </span>
+          </button>
+
         </div>
         <div className="">
           <p className="">This is the home page</p>
