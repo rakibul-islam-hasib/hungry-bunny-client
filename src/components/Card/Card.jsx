f647c7d18a92f827bf23e2e806600ecd777d1e53
Hasib Hossain <boraborhasib@gmail.com>
2023-08-11 16:28:22 +0600

    Some updated

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaShoppingCart } from "react-icons/fa";
 
 const fakeData = [
   {
@@ -47,29 +48,27 @@ const fakeData = [
 
 const Card = () => {
   return (
-    <div className="mx-auto lg:px-20 px-10 my-11  mt-10">
+    <div className="mx-auto px-20 mt-10">
       <div className="text-center lg:w-[70%] w-full sm:w-[90%] md:w-[80%] mx-auto">
         <h3 className="font-semibold text-4xl mx-auto ">Popular Menu</h3>
         <p className="font-semibold mt-2">
           Discover the essence of our culinary artistry with our signature dishes. From the savoriness of our perfectly grilled steaks to the delicate balance of flavors in our artisanal pasta, these creations are the epitome of gastronomic excellence.
         </p>
       </div>
-      <div className="grid md:grid-cols-2  lg:grid-cols-3 mt-10 md:space-x-7 mx-auto md:gap-y-8">
+      <div className="grid grid-cols-3 mt-10 mx-auto gap-y-8">
         {fakeData.map((item) => (
-          <div key={item.id} className="shadow-lg rounded-md">
-            <img className="w-full" src={item.image} alt="Food Image" />
-            <div className="px-6 py-4 relative">
+          <div key={item.id} className="max-w-sm overflow-hidden shadow-lg rounded-md">
+            <img className="w-full h-48 object-cover" src={item.image} alt="Food Image" />
+            <div className="px-6 py-4">
               <div className="font-bold text-xl mb-2">{item.name}</div>
               <p className="text-gray-700 text-base">{item.description}</p>
-
-              <div className="absolute  right-2 top-1">
-                <span className="inline-block bg-orange-300 mt-4 rounded-full px-3 py-1 text-right text-sm font-semibold text-gray-700">
-                  <p>{item.price} Taka</p>
-                </span>
+              <div className="flex justify-between items-center mt-4">
+                <span className="text-gray-600 font-semibold text-lg">{item.price} Taka</span>
+                <button className="bg-orange-500 hover:bg-orange-600 text-white font-bold py-2 px-4 rounded-full flex items-center">
+                  <FaShoppingCart className="mr-2" />
+                  Add to Cart
+                </button>
               </div>
-              <button className="my-4 bg-orange-300 rounded-full py-2 px-4 text-white font-semibold hover:bg-orange-400 transition duration-300 ease-in-out">
-                Buy Now
-              </button>
             </div>
           </div>
         ))}
--- src/router/router.jsx
+++ src/router/router.jsx
@@ -2,6 +2,8 @@ import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../layout/MainLayout";
 import Home from "../pages/Home/Home";
 import SignIn from "../pages/SignIn/SignIn";
+import About from "../pages/about/About";
+import Contact from "../pages/contact/Contact";
 
 export const router = createBrowserRouter([
   {
@@ -13,9 +15,18 @@ export const router = createBrowserRouter([
         element: <Home />,
       },
       {
-        path: "/signin",
+        path: "/login",
         element: <SignIn></SignIn>,
       },
+      {
+        path: '/about',
+        element: <About />
+      },
+      {
+        path: '/contact',
+        element: <Contact />
+      }
+
     ],
   },
 ]);
