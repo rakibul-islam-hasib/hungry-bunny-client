ee64db7bb9527322a51bda03514de3acf891bb14
Hasib Hossain <boraborhasib@gmail.com>
2023-08-15 23:44:29 +0600

    Redux setup added for testing  startup

---
+++ src/redux/store/authThunks.js
@@ -0,0 +1,28 @@
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from 'firebase/auth';
+import { setUser, setError, setLoading } from './authSlice';
+import { getAuth } from 'firebase/auth';
+import { app } from '../../config/firebase/firebase.config';
+
+const auth = getAuth(app);
+export const registerUser = (email, password) => async (dispatch) => {
+    dispatch(setLoading(true));
+    try {
+        const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+        dispatch(setUser(userCredential.user));
+    } catch (error) {
+        dispatch(setError(error.code));
+    }
+    dispatch(setLoading(false));
+};
+
+// Similar thunks for other actions...
+export const loginUser = (email, password) => async (dispatch) => {
+    dispatch(setLoading(true));
+    try {
+        const userCredential = await signInWithEmailAndPassword(auth, email, password);
+        dispatch(setUser(userCredential.user));
+    }
+    catch (error) {
+        dispatch(setError(error.code));
+    }
+}
