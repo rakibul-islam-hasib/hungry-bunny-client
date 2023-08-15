f1fcc300b854562b0ee5612fa652919ed28a324f
Hasib Hossain <boraborhasib@gmail.com>
2023-08-15 18:15:49 +0600

    Nafis vai code added

--- src/pages/SignUp/Register.jsx
+++ src/pages/SignUp/Register.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const SignUp = () => {
+const Register = () => {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <div className="bg-white p-8 shadow-md rounded-md w-96">
@@ -53,4 +53,4 @@ const SignUp = () => {
   );
 };
 
-export default SignUp;
+export default Register;
--- src/router/router.jsx
+++ src/router/router.jsx
@@ -6,6 +6,7 @@ import About from "../pages/about/About";
 import Contact from "../pages/contact/Contact";
 import CommunityLayout from "../layout/CommunityLayout";
 import Community from "../pages/community/Community";
+import Register from "../pages/SignUp/Register";
 
 export const router = createBrowserRouter([
   {
@@ -27,8 +28,11 @@ export const router = createBrowserRouter([
       {
         path: '/contact',
         element: <Contact />
-      }
-
+      },
+      {
+        path: "/register",
+        element: <Register />,
+      },
     ],
   },
   {
