1e843d15c3161c44753d3f40e523437aa2bd1430
Hasib Hossain <boraborhasib@gmail.com>
2023-08-16 17:45:47 +0600

    Updated

--- src/hooks/useAuth.jsx
+++ src/hooks/useAuth.jsx
@@ -1,5 +1,14 @@
-
+import { useSelector } from "react-redux";
+import { setUser , setError , setLoading } from "../redux/slices/authSlice";
 
 export const useAuth = () => {
-
+    const { user, error, loading } = useSelector(state => state.auth);
+    const returnObj = {
+        user,
+        error,
+        loading,
+        setUser,
+        
+    }
+    return returnObj;
 }
--- src/layout/CommunityLayout.jsx
+++ src/layout/CommunityLayout.jsx
@@ -1,23 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import CnavBar from '../components/header/Community/CnavBar';
-import { Outlet } from 'react-router-dom';
+import { Outlet, ScrollRestoration } from 'react-router-dom';
 import CSideBar from '../components/sidebar/CSideBar';
-
+import { useAuth } from '../hooks/useAuth';
+import Loader from '../components/loader/Loader';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { app } from '../config/firebase/firebase.config';
+import { setLoading, setUser } from '../redux/slices/authSlice';
+import { useDispatch } from 'react-redux';
 const CommunityLayout = () => {
+    const { loading } = useAuth();
+
+    if (loading) return <Loader />
     return (
-        <section className=' h-full'>
-            <CnavBar />
-            <section className='max-w-screen-xl mx-auto'>
-                <div className="grid md:grid-cols-5">
-                    <div className="col-span-4">
-                        <Outlet />
+        <>
+            <section className=' h-full'>
+                <CnavBar />
+                <section className='max-w-screen-xl mx-auto'>
+                    <div className="grid md:grid-cols-5">
+                        <div className="col-span-4">
+                            <Outlet />
+                        </div>
+                        <div className="col-span-1 hidden md:block">
+                            <CSideBar />
+                        </div>
                     </div>
-                    <div className="col-span-1 hidden md:block">
-                        <CSideBar />
-                    </div>
-                </div>
+                </section>
             </section>
-        </section>
+            <ScrollRestoration />
+        </>
     );
 };
 
