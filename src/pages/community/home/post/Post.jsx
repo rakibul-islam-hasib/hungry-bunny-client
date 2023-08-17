bb587260ece43d3abfc69de7cdbee70c035f4e2e
Hasib Hossain <boraborhasib@gmail.com>
2023-08-17 21:03:48 +0600

    Post page updated

--- src/pages/community/home/post/Post.jsx
+++ src/pages/community/home/post/Post.jsx
@@ -1,18 +1,36 @@
 import React from 'react';
 import moment from 'moment';
+import { AiTwotoneLike } from 'react-icons/ai';
+import { BiCommentDetail } from 'react-icons/bi';
+import { BsShareFill } from 'react-icons/bs';
 const Post = ({ post: data }) => {
     console.log(data);
     return (
-        <div className='border bg-gray-100 my-3 px-8 py-5'>
+        <div className='shadow bg-gray-100 my-3 px-8 py-5'>
             <div className="">
                 <div className="flex items-center gap-2">
                     <img src={data.user.photo} className='w-[40px] rounded-full h-[40px]' alt="" />
                     <div className="">
-                        <h1 className="font-bold mb-0 leading-3 text-lg">{data.user.name}</h1>
+                        <h1 className="font-bold mb-0 mt-2 leading-3 text-lg">{data.user.name}</h1>
                         <p className='mt-0 text-primary text-[12px]'>{moment(data.posted).format("MMM Do YY")}</p>
                     </div>
                 </div>
-                <div className=""></div>
+                <div className="border px-6 py-4  mt-4">
+                    <p>{data.post}</p>
+                </div>
+                <div className="flex justify-between mt-6 w-[80%] mx-auto">
+                    <div className="flex items-center">
+                        <AiTwotoneLike className='text-3xl cursor-pointer hover:text-primary duration-300' />
+                        <h1 className='text-lg'>{data.likes}</h1>
+                    </div>
+                    <div className="flex items-center">
+                        <BiCommentDetail className='text-3xl cursor-pointer hover:text-primary duration-300' />
+                        <h1 className='text-lg'>{data.comments.length}</h1>
+                    </div>
+                    <div className="">
+                        <BsShareFill className='text-3xl cursor-pointer hover:text-primary duration-300' />
+                    </div>
+                </div>
             </div>
         </div>
     );
