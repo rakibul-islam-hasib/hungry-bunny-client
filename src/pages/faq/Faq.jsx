0754f609b8ef31c99cbf8089557b2a9014f70978
tufaelkhan <tufaelkhan247@gmail.com>
2023-08-17 05:58:13 -0700

    add faq section

--- src/pages/faq/Faq.jsx
+++ src/pages/faq/Faq.jsx
@@ -9,14 +9,23 @@ const Faq = () => {
         .then(res => res.json())
         .then(data => {
             setAllQuestions(data)
-            console.log(data.answers);
+            console.log(data);
         })
     },[])
     return (
-        <div>
-            {/* {
-                allQuestions.map((item) => )
-            } */}
+        <div className='mb-10'>
+            <h2 className='text-4xl text-orange-500 p-6 text-center font-extrabold'>Frequently Asked Questions</h2>
+            {
+                allQuestions.map((item) =>  <div key={item._id} className="card">
+                <Accordion activeIndex={0}>
+                    <AccordionTab className='mt-3 mb-3 text-2xl hover:bg-orange-400 p-3 rounded-lg hover:text-white hover:text-4xl' header={item.question}>
+                        <p className="m-0">
+                            {item.answers}
+                        </p>
+                    </AccordionTab>
+                </Accordion>
+            </div>)
+            }
         </div>
     );
 };
