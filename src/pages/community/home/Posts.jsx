5dc37ca6f85c249aeb849ac4f47ea1f8198d114e
Hasib Hossain <boraborhasib@gmail.com>
2023-08-17 15:27:54 +0600

    Updated

--- src/pages/community/home/Posts.jsx
+++ src/pages/community/home/Posts.jsx
@@ -1,9 +1,47 @@
 import React from 'react';
+import Post from './post/Post';
 
 const Posts = () => {
+    const fakePostData = [
+        {
+            id: 1,
+            post: 'This is a simple testing post we are working to make this post title',
+            posted: 'Wed Aug 16 2023 23:21:44 GMT+0600 (Bangladesh Standard Time)',
+            Likes: 10,
+            comments: [
+                {
+                    _id: 1,
+                    comment: 'This is the comment of the post',
+                    commented: 'Wed Aug 16 2023 23:21:44 GMT+0600 (Bangladesh Standard Time)',
+                    user: {
+                        name: 'John Doe',
+                        isVerified: true,
+                        photo: 'https://i.ibb.co/51wz2Fn/pexels-the-lazy-artist-gallery-2527939.jpg'
+                    }
+                },
+                {
+                    _id: 2,
+                    comment: 'This is the comment of the post',
+                    commented: 'Wed Aug 16 2023 23:21:44 GMT+0600 (Bangladesh Standard Time)',
+                    user: {
+                        name: 'John Doe',
+                        isVerified: true,
+                        photo: 'https://i.ibb.co/51wz2Fn/pexels-the-lazy-artist-gallery-2527939.jpg'
+                    }
+                },
+            ],
+            user: {
+                name: 'John Doe',
+                isVerified: true,
+                photo: 'https://i.ibb.co/51wz2Fn/pexels-the-lazy-artist-gallery-2527939.jpg'
+            }
+        }
+    ]
     return (
         <div>
-            <h1>Post</h1>
+            {
+                fakePostData.map(post => <Post key={post.id} post={post} />)
+            }
         </div>
     );
 };
