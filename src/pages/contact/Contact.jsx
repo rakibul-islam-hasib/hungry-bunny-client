523fdeecc226a43c7166896891bee83752113827
tufaelkhan <tufaelkhan247@gmail.com>
2023-08-18 09:10:09 -0700

    add icon

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -2,6 +2,7 @@ import React, { useRef } from 'react';
 import emailjs from '@emailjs/browser';
 import Lottie from 'react-lottie';
 import animationData from '../../../src/assets/icons/animation_llczs6cf.json'
+import { FaMobileAlt, FaMapMarkerAlt, FaRocketchat } from "react-icons/fa";
 
 const Contact = () => {
 
@@ -32,9 +33,9 @@ const Contact = () => {
       <p className='text-center'>Have a question or feedback about our food delivery services?  We&apos;re here to help! Our dedicated customer <br /> support team is available to assist you with any inquiries you may have. Whether you need assistance with <br /> your order, have dietary preferences, or want to share your thoughts, feel free to reach out to <br /> us. Your satisfaction is our priority, and we&apos;re committed to providing you with a seamless and <br /> delightful food delivery experience. Contact us today and let us serve you better.</p>
       <div className='flex justify-evenly mb-10'>
 
-        <h3 className='flex  items-center text-2xl font-sans mt-5'><span className='mr-3'>icon</span> <span>(+880) 1880107...</span></h3>
-        <h3 className='flex  items-center text-2xl font-serif mt-5'><span className='mr-3'>icon</span> <span><a href="" target='blank'>Bashundara road 120/A</a></span></h3>
-        <h3 className='flex  items-center text-2xl font-serif mt-5'><span className='mr-3'>icon</span> <span><a href="" target='blank'>community</a></span></h3>
+        <h3 className='flex  items-center text-2xl font-sans mt-5'><span className='mr-3 '><FaMobileAlt className='text-orange-500'></FaMobileAlt> </span> <span>(+880) 1880107...</span></h3>
+        <h3 className='flex  items-center text-2xl font-serif mt-5'><span className='mr-3'> </span><FaMapMarkerAlt className='text-orange-500'></FaMapMarkerAlt> <span><a href="" target='blank'>Bashundara road 120/A</a></span></h3>
+        <h3 className='flex  items-center text-2xl font-serif mt-5'><span className='mr-3'><FaRocketchat className='text-orange-500'></FaRocketchat> </span> <span><a href="" target='blank'>community</a></span></h3>
       </div>
       <div className='md:flex w-full'>
 
