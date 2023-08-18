a88d1b39da23b913de271581f3bcc5aa2d2d12b1
Sujan-Ahammad <sadnansujan23@gmail.com>
2023-08-18 23:59:15 +0600

    Suja: User Dashboard name Implement

--- src/layout/dashboard/userDashboard/UserDashboard.jsx
+++ src/layout/dashboard/userDashboard/UserDashboard.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 const UserDashboard = () => {
     return (
         <div>
+            <h1>Profile</h1>
             <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, quia! Illum nam asperiores fugit corporis omnis voluptatem fuga hic incidunt aliquam, fugiat perferendis natus numquam molestiae a iusto explicabo eveniet!</h1>
+
         </div>
     );
 };
--- src/pages/Menu/FoodCard.jsx
+++ src/pages/Menu/FoodCard.jsx
@@ -11,12 +11,7 @@ const FoodCard = () => {
   const [menuTab, setMenuTab] = useState('Breakfast')
   const [loading, setLoading] = useState(false)
   const [foods] = useFetch();
-<<<<<<< HEAD
-
   
-=======
-  console.log(foods);
->>>>>>> 05bf245f524030a6fb0875ae0b2f603099ec5451
   //loading 
   useEffect(() => {
     setLoading(true)
@@ -29,12 +24,7 @@ const FoodCard = () => {
   const handleMenuTabs = (type) => {
     setMenuTab(type)
   }
-<<<<<<< HEAD
   
-=======
-
-
->>>>>>> 05bf245f524030a6fb0875ae0b2f603099ec5451
   return (
     <div className="mx-auto md:px-20 px-6 mt-10  #F4ECE1 #FCFBF7">
       <div className="text-center lg:w-[70%] w-full sm:w-[90%] md:w-[80%] mx-auto">
