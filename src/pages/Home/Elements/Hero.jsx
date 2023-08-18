73f3188bf4e47fa0f785f3cdd1750ba547d10d05
Hasib Hossain <boraborhasib@gmail.com>
2023-08-18 16:45:57 +0600

    Hero updated

--- src/pages/Home/Elements/Hero.jsx
+++ src/pages/Home/Elements/Hero.jsx
@@ -6,30 +6,31 @@ import fastFood from '../../../assets/img/fast-food.png';
 const Hero = () => {
   return (
     <section className='h-screen'>
-      <div className="grid mg:grid-cols-2 lg:grid-cols-2 w-[90%] mx-auto">
-        <div className="flex flex-col mt-14 justify-center items-start h-full space-y-4">
-          <div className="flex">
-            <h1 className='text-8xl text-orange-500 font-bold'>Fast</h1>
-            <div className="">
-              <h1 className='text-4xl font-bold'>Food</h1>
-              <h1 className='text-4xl font-bold'>Delivery</h1>
+      <div className=" w-[90%] mx-auto">
+        <div className=" mt-14   h-full space-y-4">
+
+          <div className="logo text-center">
+            <h1 className='text-8xl font-bold dark:text-gray-100'><span className='text-primary'>Hungry</span> Bunny</h1>
+          </div>
+          <div className="flex justify-center items-center">
+            <h1 className='text-5xl text-orange-500 font-bold'>Fast</h1>
+            <div className="dark:text-white leading-4 text-black">
+              <h1 className='text-xl font-bold leading-3'>Food</h1>
+              <h1 className='text-xl font-bold'>Delivery</h1>
             </div>
           </div>
-          <p className='text-gray-500'>Imagine your hunger being met at warp speed. Our &quot;Supper First Delivery&quot; isn&apos;t just about delivering food; it&apos;s about delivering delight.</p>
+          <p className='text-gray-500 text-center'>Imagine your hunger being met at warp speed. Our &quot;Supper First Delivery&quot; isn&apos;t just about delivering food; it&apos;s about delivering delight.</p>
 
-          <div className="flex gap-3">
-            <button className=' px-7 py-4 text-xl font-bold text-white rounded-full bg-orange-500 '>Order Now</button>
-            <button className='flex justify-center items-center text-xl font-bold'>
-              <img src={playIcon} className='h-16 w-16' alt="" />
-              <span>Watch Video</span>
-            </button>
+          {/* Search Box */}
+          <div className="mt-6 flex  items-center w-[70%] mx-auto">
+            <input
+              type="text"
+              placeholder="Search for products, services, and more"
+              className="px-4 py-4 text-xl border outline-none  border-gray-300 rounded-md focus:ring-orange-500 focus:border-orange-500 w-full" />
+            {/* <button className="px-4 py-2 text-white font-bold bg-orange-500 rounded-md hover:bg-orange-600 focus:outline-none focus:ring focus:ring-orange-300 focus:ring-opacity-50">
+              Search
+            </button> */}
           </div>
-
-
-
-        </div>
-        <div className="mt-20">
-          <img src={fastFood} alt="" />
         </div>
       </div>
     </section>
