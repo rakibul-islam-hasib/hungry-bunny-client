2fd1906230870dcafce6a4e7279df65948ea7ee7
Hasib Hossain <boraborhasib@gmail.com>
2023-08-19 21:26:39 +0600

    Dashboard updated #22

--- src/pages/dashboard/user/UserProfile.jsx
+++ src/pages/dashboard/user/UserProfile.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { motion } from 'framer-motion';
 import { Dialog, Transition } from '@headlessui/react';
+import { MdDriveFileRenameOutline } from 'react-icons/md';
 
 const UserProfile = () => {
     const [isOpen, setIsOpen] = useState(false);
@@ -19,19 +20,59 @@ const UserProfile = () => {
                 <div className="">
                     <h1 className='text-2xl'>Photo : </h1>
                     <img className='h-[200px] w-[200px]' src="https://i.ibb.co/5M0BDy4/No-thing.png" alt="" />
+                    <button
+                        onClick={openModal}
+                        className="px-3 mt-2 -py-1 bg-orange-500 text-white rounded hover:bg-orange-600"
+                    >
+                        Change Photo
+                    </button>
+
                 </div>
                 <div className="mt-6">
                     <h1 className='text-base'>Name : </h1>
-                    <p className='text-2xl'>Hasib Hossain</p>
+                    <div className="flex items-center gap-3">
+                        <p className='text-2xl'>Hasib Hossain</p>
+                        <button
+                            onClick={openModal}
+                            className="px-1 py-1 bg-orange-500 text-white rounded hover:bg-orange-600"
+                        >
+                            <MdDriveFileRenameOutline className='text-xl' />
+                        </button>
+                    </div>
+
                 </div>
                 <div className="mt-6">
                     <h1 className='text-base'>Email : </h1>
-                    <p className='text-2xl'>boraborhasib@gmail.com</p>
+                    <div className="flex items-center gap-3">
+                        <p className='text-2xl'>boraborhasib@gmail.com</p>
+                        <button
+                            onClick={openModal}
+                            className="px-1 py-1 bg-orange-500 text-white rounded hover:bg-orange-600"
+                        >
+                            <MdDriveFileRenameOutline className='text-xl' />
+                        </button>
+                    </div>
                 </div>
                 <div className="mt-6">
                     <h1 className='text-base'>Phone : </h1>
-                    <p className='text-2xl'>1 : 01319373523</p>
-                    <p className='text-2xl'>2 : 01319373523</p>
+                    <div className="flex items-center gap-3">
+                        <p className='text-2xl'>1 : 01319373523</p>
+                        <button
+                            onClick={openModal}
+                            className="px-1 py-1 bg-orange-500 text-white rounded hover:bg-orange-600"
+                        >
+                            <MdDriveFileRenameOutline className='text-xl' />
+                        </button>
+                    </div>
+                    <div className="flex items-center gap-3">
+                        <p className='text-2xl'>2 : 01319373523</p>
+                        <button
+                            onClick={openModal}
+                            className="px-1 py-1 bg-orange-500 text-white rounded hover:bg-orange-600"
+                        >
+                            <MdDriveFileRenameOutline className='text-xl' />
+                        </button>
+                    </div>
                 </div>
                 <div className="mt-6">
                     <h1 className='text-base'>Address : </h1>
@@ -39,7 +80,7 @@ const UserProfile = () => {
                 </div>
                 <div className="mt-6">
                     <h1 className='text-base'> : </h1>
-                    
+
                 </div>
             </div>
 
@@ -50,13 +91,15 @@ const UserProfile = () => {
                         className="fixed inset-0 z-10 overflow-y-auto"
                         onClose={closeModal}
                     >
-                        <div className="flex items-center justify-center min-h-screen">
+                        <div
+                            className="flex items-center justify-center min-h-screen"
+                        >
                             <Transition.Child
                                 as={motion.div}
                                 initial={{ opacity: 0, scale: 0.5 }}
                                 animate={{ opacity: 1, scale: 1 }}
-                                exit={{ opacity: 0, scale: 0.5 }}
-                                className="bg-white p-8 rounded-lg shadow-lg"
+                                exit={{ opacity: 0, scale: 0.5, transition: { duration: 0.6 } }}
+                                className="bg-white p-8 rounded-lg md:w-[300px] shadow-lg"
                             >
                                 {/* Your form components go here */}
                                 <div className="mb-4">
@@ -65,11 +108,26 @@ const UserProfile = () => {
                                     </label>
                                     <input
                                         type="text"
-                                        className="w-full px-3 py-2 placeholder-gray-400 border rounded-lg focus:ring-orange-500 focus:border-orange-500"
+                                        className="w-full outline-none px-3 py-2 placeholder-gray-400 border rounded-lg focus:ring-orange-500 focus:border-orange-500"
+                                    />
+                                </div>
+                                <div className="mb-4">
+                                    <label className="block text-gray-700 font-bold mb-2">
+                                        Profile Name
+                                    </label>
+                                    <input
+                                        type="text"
+                                        className="w-full outline-none px-3 py-2 placeholder-gray-400 border rounded-lg focus:ring-orange-500 focus:border-orange-500"
                                     />
                                 </div>
                                 {/* ... other form fields ... */}
                                 <div className="flex justify-end mt-6">
+                                    <button
+                                        onClick={closeModal}
+                                        className="px-4 py-2 mr-2 bg-gray-200 text-gray-600 rounded hover:bg-gray-300"
+                                    >
+                                        Cancel
+                                    </button>
                                     <button
                                         onClick={closeModal}
                                         className="px-4 py-2 bg-orange-500 text-white rounded hover:bg-orange-600"
