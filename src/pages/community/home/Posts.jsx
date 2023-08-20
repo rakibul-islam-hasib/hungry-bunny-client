e6f93a8cb4ecc697966c7c78a1e3123162338b1c
Hasib Hossain <boraborhasib@gmail.com>
2023-08-20 21:01:11 +0600

    Changed

--- src/pages/community/home/Posts.jsx
+++ src/pages/community/home/Posts.jsx
@@ -1,21 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Post from './post/Post';
-import useAxiosFetch from '../../../hooks/useAxiosFetch';
 import { useCommunityPost } from '../../../hooks/data/useCommunityPost';
 
 const Posts = () => {
-    // const [postData, setPostData] = useState([]);
-    // const [loading, setLoading] = useState(true);
-    const axios = useAxiosFetch();
     const [postData, loading, refetch] = useCommunityPost();
-    // useEffect(() => {
-    //     const fetchPosts = async () => {
-    //         const res = await axios.get('/community-post');
-    //         setPostData(res.data);
-    //         setLoading(false);
-    //     }
-    //     fetchPosts();
-    // }, [axios])
     if (loading) return <h1>Loading...</h1>
     return (
         <div>
