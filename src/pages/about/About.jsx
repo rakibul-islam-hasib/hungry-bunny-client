85d7f564a315321857c01ec1c842a4b6acb88324
tufaelkhan <tufaelkhan247@gmail.com>
2023-08-21 04:06:35 -0700

    add new image

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -49,7 +49,7 @@ const About = () => {
       <div className='mt-7 md:flex mb-4'>
         <div className='mt-6 w-[50%] mr-8'>
           <h3 className='font-extrabold text-4xl'>Empowering Culinary Creativity, One Delivery at a Time</h3>
-          <p className='mt-4'>Join our [Your Food Delivery Partner Name] family and embark on a journey that celebrates culinary excellence. As a trusted delivery partner, we bridge the gap between talented chefs and eager diners. From kitchen to doorstep, we ensure that every dish retains its essence and arrives with care. Discover a new dimension of flavors with Your Food Delivery Partner Name, where passion for food meets seamless delivery.</p>
+          <p className='mt-4'>Join our Your Food Delivery Partner Name family and embark on a journey that celebrates culinary excellence. As a trusted delivery partner, we bridge the gap between talented chefs and eager diners. From kitchen to doorstep, we ensure that every dish retains its essence and arrives with care. Discover a new dimension of flavors with Your Food Delivery Partner Name, where passion for food meets seamless delivery.</p>
           <button className="bg-orange-600 hover:bg-orange-700 mt-8 text-white font-bold py-2 px-4 rounded-full flex items-center">
             become a partner
           </button>
@@ -81,7 +81,7 @@ const About = () => {
           </button>
         </div>
         <div className='w-[50%]'>
-          <img style={{ height: 400 }} className='rounded-lg' src="https://previews.123rf.com/images/yupiramos/yupiramos1506/yupiramos150603691/40932571-food-delivery-design.jpg" alt="" />
+          <img style={{ height: 400 }} className='rounded-lg' src="https://images.pexels.com/photos/4392039/pexels-photo-4392039.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
         </div>
       </div>
     </div>
