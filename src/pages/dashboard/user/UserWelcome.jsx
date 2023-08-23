0757c63c45ac60e83544c10d7ad4beadb459512a
tufaelkhan <tufaelkhan247@gmail.com>
2023-08-22 19:54:35 -0700

    add user dashboard

--- src/pages/dashboard/user/UserWelcome.jsx
+++ src/pages/dashboard/user/UserWelcome.jsx
@@ -2,8 +2,32 @@ import React from 'react';
 
 const UserWelcome = () => {
     return (
-        <div>
-            <h1>Welcome to Hungry Bunny</h1>
+        <div className='mt-10'>
+            <h1 className='text-4xl text-orange-500 font-bold'>Welcome to Hungry Bunny user name</h1>
+<div className='flex justify-between mr-9 mb-7'>
+<h1 className='text-2xl mt-4 font-bold'>your added card items</h1>
+<button className="bg-orange-500 text-2xl hover:bg-orange-600 text-white font-bold py-2 px-4 rounded-full flex restaurants-center">
+                payment
+              </button>
+</div>
+            <table className="w-full text-left">
+  <thead>
+    <tr>
+      <th>#</th>
+      <th>name of food</th>
+      <th>price</th>
+      <th>status</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>The Sliding Mr. Bones </td>
+      <td>Malcolm Lockyer</td>
+      <td>1961</td>
+      <td>pending</td>
+    </tr>
+  </tbody>
+</table>
         </div>
     );
 };
