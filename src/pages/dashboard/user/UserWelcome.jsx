067ccf7bf44cb7534c2de662e51e508fb92e16cb
tufaelkhan <tufaelkhan247@gmail.com>
2023-08-23 04:19:08 -0700

    add user dashboard

--- src/pages/dashboard/user/UserWelcome.jsx
+++ src/pages/dashboard/user/UserWelcome.jsx
@@ -3,31 +3,14 @@ import React from 'react';
 const UserWelcome = () => {
     return (
         <div className='mt-10'>
-            <h1 className='text-4xl text-orange-500 font-bold'>Welcome to Hungry Bunny user name</h1>
-<div className='flex justify-between mr-9 mb-7'>
-<h1 className='text-2xl mt-4 font-bold'>your added card items</h1>
-<button className="bg-orange-500 text-2xl hover:bg-orange-600 text-white font-bold py-2 px-4 rounded-full flex restaurants-center">
-                payment
-              </button>
+            <h1 className='text-4xl text-orange-500 font-bold'>Welcome back Hungry Bunny</h1>
+<div className='flex justify-between mr-9 mb-7 mt-10 space-x-10'>
+<div className='text-center bg-gradient-to-r from-cyan-500 to-blue-500 pt-8 pb-8 pr-16 pl-16 rounded-2xl text-white'>
+  <p className='text-6xl'>number</p>
+  <p className='text-4xl font-bold'><span>icon</span> <span>MENU</span></p>
 </div>
-            <table className="w-full text-left">
-  <thead>
-    <tr>
-      <th>#</th>
-      <th>name of food</th>
-      <th>price</th>
-      <th>status</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td>The Sliding Mr. Bones </td>
-      <td>Malcolm Lockyer</td>
-      <td>1961</td>
-      <td>pending</td>
-    </tr>
-  </tbody>
-</table>
+</div>
+
         </div>
     );
 };
