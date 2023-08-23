ec6543b3d45132b02e69a5902e3700e93eec07c3
Hasib Hossain <boraborhasib@gmail.com>
2023-08-23 18:47:59 +0600

    Modal updated

--- src/pages/community/Elements/CommentModal.jsx
+++ src/pages/community/Elements/CommentModal.jsx
@@ -1,13 +1,12 @@
 import React, { useState } from 'react';
-import { Dialog, Transition } from '@headlessui/react';
+import { Dialog, Transition, FocusTrap } from '@headlessui/react';
 import { motion } from 'framer-motion';
 import './css/Style.css'
+import { IoCloseSharp } from 'react-icons/io5';
 
 const PostModal = ({ isOpen, onClose, onSuccess, data, postId }) => {
 
     console.log(postId === data._id ? data?.comments : '', 'data from PostModal')
-
-
 
     return (
         <Transition.Root show={isOpen} as={React.Fragment}>
@@ -27,40 +26,48 @@ const PostModal = ({ isOpen, onClose, onSuccess, data, postId }) => {
                         className="bg-white p-8 rounded-lg md:min-w-[500px] shadow-lg"
                     >
                         {/* Your form components go here */}
-                        <div className="h-[300px] comment-section overflow-y-auto">
-                            {
-                                data?.comments?.map((comment, i) => (
-                                    <div key={i} className="flex items-center justify-between">
-                                        <div className="flex items-center">
-                                            <img src={comment?.user?.photo} alt="" className='w-8 h-8 rounded-full' />
-                                            <div className="ml-2">
-                                                <p className='font-bold'>{comment?.user?.name}</p>
-                                                <p>{comment?.comment}</p>
+                        <FocusTrap>
+                            <button tabIndex={0} style={{ position: 'absolute', left: '-9999px' }}></button>
+                            <div className="relative">
+                                <div className="absolute -right-6 -top-6 p-2 bg-red-500 rounded-xl ">
+                                    <IoCloseSharp onClick={onClose} className="text-2xl text-white cursor-pointer" />
+                                </div>
+                                <div className="h-[300px] border p-4 rounded-xl comment-section overflow-y-auto">
+                                    {
+                                        data?.comments?.map((comment, i) => (
+                                            <div key={i} className="flex items-center justify-between">
+                                                <div className="flex items-center">
+                                                    <img src={comment?.user?.photo} alt="" className='w-8 h-8 rounded-full' />
+                                                    <div className="ml-2">
+                                                        <p className='font-bold'>{comment?.user?.name}</p>
+                                                        <p>{comment?.comment}</p>
+                                                    </div>
+                                                </div>
+                                                <div className="flex items-center gap-2">
+                                                    <p className='text-xs text-gray-400'>{comment?.createdAt}</p>
+                                                    <p className='text-xs text-gray-400'>Reply</p>
+                                                </div>
                                             </div>
-                                        </div>
-                                        <div className="flex items-center gap-2">
-                                            <p className='text-xs text-gray-400'>{comment?.createdAt}</p>
-                                            <p className='text-xs text-gray-400'>Reply</p>
-                                        </div>
-                                    </div>
-                                ))
-                            }
-                        </div>
-                        {/* ... other form fields ... */}
-                        <div className="flex justify-end mt-6">
-                            <button
-                                onClick={onClose}
-                                className="px-4 py-2 mr-2 bg-gray-200 text-gray-600 rounded hover:bg-gray-300"
-                            >
-                                Cancel
-                            </button>
-                            <button
-                                onClick={onSuccess}
-                                className="px-4 py-2 bg-orange-500 text-white rounded hover:bg-orange-600"
-                            >
-                                Ok Delete
-                            </button>
-                        </div>
+                                        ))
+                                    }
+                                </div>
+                            </div>
+                            {/* ... other form fields ... */}
+                            {/* <div className="flex justify-end mt-6">
+                                <button
+                                    onClick={onClose}
+                                    className="px-4 py-2 mr-2 bg-gray-200 text-gray-600 rounded hover:bg-gray-300"
+                                >
+                                    Cancel
+                                </button>
+                                <button
+                                    onClick={onSuccess}
+                                    className="px-4 py-2 bg-orange-500 text-white rounded hover:bg-orange-600"
+                                >
+                                    Ok Delete
+                                </button>
+                            </div> */}
+                        </FocusTrap>
                     </Transition.Child>
                 </div>
             </Dialog>
