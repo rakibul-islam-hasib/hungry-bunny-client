434429c2ae2671c11fdac5836ff931b69d3e93bf
rater7tara <nowshinakteremu005@gmail.com>
2023-08-24 13:42:38 +0600

    owner dashboard file create

---
+++ src/pages/dashboard/Owner/OwnerProfile.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const OwnerProfile = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default OwnerProfile;
---
+++ src/pages/dashboard/Owner/OwnerWelcome.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const OwnerWelcome = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default OwnerWelcome;
