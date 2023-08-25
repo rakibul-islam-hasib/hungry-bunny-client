0add6c838f1910dcc67cb6b0a58f998d09fcf1da
Hasib Hossain <boraborhasib@gmail.com>
2023-08-25 17:00:24 +0600

    All new updated

--- src/pages/temp/Temp.jsx
+++ src/pages/temp/Temp.jsx
@@ -1,9 +1,66 @@
 import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import './Temp.css';
 
+import { EffectCoverflow, Pagination, Navigation } from 'swiper/modules';
 const Temp = () => {
   return (
     <div>
-      
+      <Swiper
+        effect={'coverflow'}
+        loop={true}
+        
+        grabCursor={true}
+        centeredSlides={true}
+        navigation={{ nextEl: '.next-swp', prevEl: '.prv-swp' }}
+        slidesPerView={'auto'}
+        coverflowEffect={{
+          rotate: 0,
+          stretch: 5,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
+        }}
+        pagination={true}
+        modules={[EffectCoverflow, Pagination, Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-5.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-6.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-7.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-8.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-9.jpg" />
+        </SwiperSlide>
+      </Swiper>
+      <div className="">
+        <button className='next-swp'>Next</button>
+        <button className='prv-swp'>Prv</button>
+      </div>
     </div>
   );
 };
