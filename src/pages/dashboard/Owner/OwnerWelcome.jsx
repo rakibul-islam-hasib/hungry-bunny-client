58f1a333aa4b55c89072961fb51bc7f055b2f946
rater7tara <nowshinakteremu005@gmail.com>
2023-08-25 20:00:17 +0600

    style change

--- src/pages/dashboard/Owner/OwnerWelcome.jsx
+++ src/pages/dashboard/Owner/OwnerWelcome.jsx
@@ -4,7 +4,7 @@ const OwnerWelcome = () => {
     return (
         <div className='mt-10 sm:text-center'>
             <h1 className='text-5xl text-orange-500 font-bold'>Welcome back Hungry Bunny</h1>
-            <h1 className='text-4xl text-black font-bold'>Owner dashboard</h1>
+            <h1 className='text-4xl text-black font-bold'>User dashboard</h1>
 
 
         </div>
