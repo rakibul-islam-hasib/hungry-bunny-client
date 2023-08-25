415d23f9a0c7296fd9fa4aab5836835fd5524f65
Project_3 <anik2115@gmail.com>
2023-08-25 17:14:20 +0600

    admin dashboard added

---
+++ src/pages/dashboard/user/RestaurantAdmin/AdminCart.jsx
@@ -0,0 +1,22 @@
+import { LineChart } from "@mui/x-charts";
+import React from "react";
+
+const AdminCart = () => {
+  return (
+    <div>
+      <LineChart
+        xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
+        series={[
+          {
+            data: [2, 5.5, 2, 8.5, 1.5, 5],
+            area: true,
+          },
+        ]}
+        width={700}
+        height={300}
+      />
+    </div>
+  );
+};
+
+export default AdminCart;
