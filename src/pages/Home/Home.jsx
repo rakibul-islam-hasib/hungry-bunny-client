e61d5dd9cf5903d08ae40746c8af124ec3586ac2
Hasib Hossain <boraborhasib@gmail.com>
2023-08-25 21:12:09 +0600

    Sm

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import Hero from './Elements/Hero';
-import CheckoutBar from '../../components/cart/CheckoutBar';
-import Slider from '../../components/Slider/Slider';
-
-const Home = () => {
-    return (
-        <section>
-            <Hero />
-            <CheckoutBar />
-            <Slider></Slider>
-        </section>
-    );
-=======
 import React from "react";
 import Hero from "./Elements/Hero";
 import CheckoutBar from "../../components/cart/CheckoutBar";
@@ -28,14 +13,13 @@ const Home = () => {
   return (
     <section>
       <Hero />
-      <TopRestaurant/>
-      <FoodCard/>
+      <TopRestaurant />
+      <FoodCard />
       <PopularFood />
       <Stacks />
       <CheckoutBar />
     </section>
   );
->>>>>>> 247d35b4a87f8287565c1163919ab29461a20eae
 };
 
 export default Home;
