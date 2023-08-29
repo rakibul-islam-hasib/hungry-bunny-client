ee49c8c3eb733107c0199c1ba962983d115cab3b
Hasib Hossain <boraborhasib@gmail.com>
2023-08-29 22:20:35 +0600

    Role managment bug fixed

--- src/layout/DashboardLayout.jsx
+++ src/layout/DashboardLayout.jsx
@@ -95,14 +95,16 @@ const DashboardLayout = () => {
   const [open, setOpen] = useState(true);
   const { user: firebaseUser } = useAuth();
   const [user, isLoading] = useUserSecure(firebaseUser?.email);
+  console.log(user)
   const role = user?.role || "user";
-
+  console.log(role)
   /* 
-    Total role list : 
-    1. admin
-    2. user
-    3. restaurant
+  Total role list : 
+  1. admin
+  2. user
+  3. restaurant
   */
+  if (!firebaseUser?.email) return <Loader />;
   if (isLoading) return <Loader />;
 
   return (
