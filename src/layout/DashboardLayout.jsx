f4154eb21951dd6cc580070d3fb1c86584261bdf
Hasib Hossain <boraborhasib@gmail.com>
2023-08-29 18:56:37 +0600

    Dynamic role done

--- src/layout/DashboardLayout.jsx
+++ src/layout/DashboardLayout.jsx
@@ -6,6 +6,9 @@ import { BsFillPostcardFill } from "react-icons/bs";
 import { TbBrandAppleArcade } from "react-icons/tb";
 import { CgProfile } from "react-icons/cg";
 import logo from '../assets/img/logo.png';
+import useUserSecure from "../hooks/useUserSecure";
+import { useAuth } from "../hooks/useAuth";
+import Loader from "../components/loader/Loader";
 
 
 const adminNavItems = [
@@ -90,28 +93,35 @@ const restaurantNavItems = [
 
 const DashboardLayout = () => {
   const [open, setOpen] = useState(true);
+  const { user: firebaseUser } = useAuth();
+  const [user, isLoading] = useUserSecure(firebaseUser?.email);
+  console.log(user)
+  const role = user?.role || "user";
 
-  const role = "admin";
+  /* 
+    Total role list : 
+    1. admin
+    2. user
+    3. restaurant
+  */
+  if (isLoading) return <Loader />;
 
   return (
     <div className="flex bg-[#f1ecea]">
       <div
-        className={`${
-          open ? "w-72 overflow-y-auto" : "w-[90px] overflow-auto"
-        }  h-screen p-5 hidden md:block pt-8 relative duration-300`}
+        className={`${open ? "w-72 overflow-y-auto" : "w-[90px] overflow-auto"
+          }  h-screen p-5 hidden md:block pt-8 relative duration-300`}
       >
         <div className="flex gap-x-4 items-center">
           <img
             src={"https://i.ibb.co/26dQJcm/musical-note.png"}
             onClick={() => setOpen(!open)}
-            className={`cursor-pointer h-[40px] duration-500 ${
-              open && "rotate-[360deg]"
-            }`}
+            className={`cursor-pointer h-[40px] duration-500 ${open && "rotate-[360deg]"
+              }`}
           />
           <h1
-            className={`text-dark-primary cursor-pointer font-bold origin-left text-xl duration-200 ${
-              !open && "scale-0"
-            }`}
+            className={`text-dark-primary cursor-pointer font-bold origin-left text-xl duration-200 ${!open && "scale-0"
+              }`}
             onClick={() => setOpen(!open)}
           >
             HUNGRY BUNNY
@@ -129,16 +139,14 @@ const DashboardLayout = () => {
                   <NavLink
                     to={menuItem.to}
                     className={({ isActive }) =>
-                      `flex ${
-                        isActive ? "bg-primary text-white " : "text-[#413F44]"
+                      `flex ${isActive ? "bg-primary text-white " : "text-[#413F44]"
                       }  duration-150 rounded-md p-2 cursor-pointer hover:bg-primary  hover:text-white  font-bold text-sm items-center gap-x-4  `
                     }
                   >
                     {menuItem.icon}
                     <span
-                      className={`${
-                        !open && "hidden"
-                      } origin-left duration-200`}
+                      className={`${!open && "hidden"
+                        } origin-left duration-200`}
                     >
                       {menuItem.label}
                     </span>
@@ -158,16 +166,14 @@ const DashboardLayout = () => {
                   <NavLink
                     to={menuItem.to}
                     className={({ isActive }) =>
-                      `flex ${
-                        isActive ? "bg-primary text-white " : "text-[#413F44]"
+                      `flex ${isActive ? "bg-primary text-white " : "text-[#413F44]"
                       }  duration-150 rounded-md p-2 cursor-pointer hover:bg-primary  hover:text-white  font-bold text-sm items-center gap-x-4  `
                     }
                   >
                     {menuItem.icon}
                     <span
-                      className={`${
-                        !open && "hidden"
-                      } origin-left duration-200`}
+                      className={`${!open && "hidden"
+                        } origin-left duration-200`}
                     >
                       {menuItem.label}
                     </span>
@@ -187,16 +193,14 @@ const DashboardLayout = () => {
                   <NavLink
                     to={menuItem.to}
                     className={({ isActive }) =>
-                      `flex ${
-                        isActive ? "bg-primary text-white " : "text-[#413F44]"
+                      `flex ${isActive ? "bg-primary text-white " : "text-[#413F44]"
                       }  duration-150 rounded-md p-2 cursor-pointer hover:bg-primary  hover:text-white  font-bold text-sm items-center gap-x-4  `
                     }
                   >
                     {menuItem.icon}
                     <span
-                      className={`${
-                        !open && "hidden"
-                      } origin-left duration-200`}
+                      className={`${!open && "hidden"
+                        } origin-left duration-200`}
                     >
                       {menuItem.label}
                     </span>
@@ -214,12 +218,11 @@ const DashboardLayout = () => {
 
           <li>
             <NavLink
-            to={'/'}
+              to={'/'}
               className={({ isActive }) =>
-                `flex ${
-                  isActive
-                    ? "bg-dark-primary-3 text-dark-primary"
-                    : "text-[#413F44]"
+                `flex ${isActive
+                  ? "bg-dark-primary-3 text-dark-primary"
+                  : "text-[#413F44]"
                 }  duration-150 rounded-md inline-flex p-2 cursor-pointer hover:bg-dark-primary-3  font-bold text-sm items-center gap-x-4  `
               }
             >
@@ -232,10 +235,9 @@ const DashboardLayout = () => {
           <li>
             <NavLink
               className={({ isActive }) =>
-                `flex ${
-                  isActive
-                    ? "bg-dark-primary-3 text-dark-primary"
-                    : "text-[#413F44]"
+                `flex ${isActive
+                  ? "bg-dark-primary-3 text-dark-primary"
+                  : "text-[#413F44]"
                 }  duration-150 rounded-md inline-flex p-2 cursor-pointer hover:bg-dark-primary-3  font-bold text-sm items-center gap-x-4  `
               }
             >
