0ea7ec96d7bcc3521ca0df7e28679b970b5c7972
tufaelkhan <tufaelkhan247@gmail.com>
2023-08-28 19:25:21 -0700

    dark emplementation blog page

--- src/pages/blog/Blog.jsx
+++ src/pages/blog/Blog.jsx
@@ -24,10 +24,20 @@ const Blog = () => {
   }, [page])
 
   return (
-    <div>
+    <div className='dark:text-white'>
+      <div className='bg-orange-50 dark:bg-gradient-to-r from-cyan-400 to-blue-300  mt-5 pt-8 pb-8 rounded-3xl drop-shadow-lg'>
+          <div className='text-center mx-auto'>
+            <h2 className='text-4xl font-extrabold mb-6 text-purple-800'>All blogs posts</h2>
+            <h2 className='text-2xl font-bold text-slate-800 dark:text-white mb-4'> Savoring Flavors, One Recipe at a Bite</h2>
+          </div>
+          <div className='md:flex ml-6 md:ml-96'>
+            <input type="text" className='rounded-2xl w-[40%] text-4xl dark:text-black' />
+            <button className='bg-orange-500 text-white pl-7 pr-7 pt-4 pb-4 text-4xl rounded-2xl ml-5'>search</button>
+          </div>
+          </div>
       <div className="grid md:grid-cols-2 md:gap-8 gap-3 lg:grid-cols-3 mt-10 mx-auto">
         {blogs.map((item) => (
-          <div key={item._id} className='p-2 border overflow-hidden shadow-lg rounded-md'>
+          <div key={item._id} className='p-2 border overflow-hidden shadow-lg rounded-md dark:drop-shadow-md'>
             <img className="w-full h-48 object-cover rounded-lg" src={item.blogImage} alt="Food Image" />
             <div className='flex justify-between mt-3'>
               <p>{item.date}</p>
@@ -43,8 +53,8 @@ const Blog = () => {
                     <img className='rounded-full mr-3' style={{ height: 40 }} src={item.authorImage} alt="" />
                   </div>
                   <div>
-                    <p className="text-gray-700 text-base">{item.authorName}</p>
-                    <p className="text-gray-700 text-base">{item.date}</p>
+                    <p className="text-gray-700 dark:text-white text-base">{item.authorName}</p>
+                    <p className="text-gray-700 dark:text-white text-base">{item.date}</p>
                   </div>
                 </div>
                 <div className="flex justify-between items-center mt-4">
--- src/pages/blog/BlogDetails.jsx
+++ src/pages/blog/BlogDetails.jsx
@@ -7,7 +7,7 @@ function BlogDetails() {
     const blogs = useLoaderData([])
     console.log(blogs);
     return (
-        <div className="mt-10 mx-auto">
+        <div className="mt-10 mx-auto dark:text-white">
             <h2 className="text-4xl ml-5 mb-7 font-extrabold mt-5">{blogs.blogHeading}</h2>
             <div>
         <img className="rounded-2xl mb-10" src={blogs.blogImage} alt="food image" />
@@ -16,7 +16,7 @@ function BlogDetails() {
         <div className="flex justify-between border-2 border-orange-400 pt-8 pb-8 pr-5 pl-5 rounded-2xl items-center text-center">
         <div className="flex">
         <img className="rounded-full mr-5" style={{height:50}} src={blogs.authorImage} alt="author image" />
-        <div className="text-left">
+        <div className="text-left text-2xl">
             <p>{blogs.authorName}</p>
             <p>{blogs.email}</p>
             <p>{blogs.date}</p>
@@ -38,8 +38,13 @@ function BlogDetails() {
         </div>
         </div>
         <div className="mt-16">
-            <h2 className="text-4xl text-center font-bold">Recent Blogs</h2>
-
+            <h2 className="text-4xl text-center font-bold mb-10">Recent Blogs</h2>
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+            <img src="https://media.istockphoto.com/id/1426039616/photo/text-faq-on-wooden-blocks-with-letters-on-a-white-background-reflection-of-the-caption-on-the.jpg?s=2048x2048&w=is&k=20&c=w_doApBfw2GzUDCfujNbshmKf918fVscTjJQnSBhWMc=" alt="" />
+            <img src="" alt="" />
+            <img src="" alt="" />
+            <img src="" alt="" />
+        </div>
         </div>
         </div>
     );
