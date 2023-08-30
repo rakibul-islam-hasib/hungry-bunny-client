45d819cb632da0c46f1048cec98dfd27ea3955fc
Anika <anika.bd1994@gmail.com>
2023-08-30 15:41:56 +0600

    data loaded

--- src/pages/Menu/AllFood/AllFood.jsx
+++ src/pages/Menu/AllFood/AllFood.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useEffect } from 'react';
 import { useState } from 'react';
+import AllFoodCard from './AllFoodCard';
 
 const AllFood = () => {
     const [menu, setMenu] = useState([]);
@@ -9,9 +10,15 @@ const AllFood = () => {
         .then(res => res.json())
         .then(data => setMenu(data))
     }, [])
+
     return (
         <div>
-            
+           {
+            menu.map(item=> <AllFoodCard
+            key={item.id}
+            item={item}
+            ></AllFoodCard>)
+           } 
         </div>
     );
 };
--- src/pages/Menu/AllFood/AllFoodCard.jsx
+++ src/pages/Menu/AllFood/AllFoodCard.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
-const AllFoodCard = () => {
+const AllFoodCard = ({item}) => {
+    const {name, image, price} = item;
     return (
         <div>
-            
+            <img src={image} alt="" />
+            <div>
+                <p>{name}</p>
+                <p>{price}</p>
+            </div>
         </div>
     );
 };
