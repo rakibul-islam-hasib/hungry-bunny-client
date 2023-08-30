8d37a7f6b4a095addb00b1b48af12ddf11b941a0
Hasib Hossain <boraborhasib@gmail.com>
2023-08-30 23:00:24 +0600

    News letter section done

--- src/components/footer/NewsLetter.jsx
+++ src/components/footer/NewsLetter.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
-
+import { GrMail } from 'react-icons/gr';
 const NewsLetter = () => {
     return (
-        <div>
-            
+        <div className='w-full py-2 bg-primary'>
+            <div className="w-[80%] flex justify-between items-center text-white dark:text-gray-200  mx-auto">
+                <div className="">
+                    <div
+                        className='text-xl inline-flex  items-center gap-2  font-bold'>
+                        <GrMail className='text-2xl mb-[2px]' /> Subscribe to our newsletter</div>
+                </div>
+                <div className="">
+                    <h1>Shopping First For Coupon $25 Receive And...</h1>
+                </div>
+                <div className="relative">
+                    <input
+                        type="email"
+                        className='w-full lg:w-[300px] dark:bg-gray-200 py-3 pl-4 text-black rounded-lg' placeholder='Your Email Address' />
+                    <button className='absolute bg-black text-white font-bold py-2 rounded-lg px-2 right-1 top-1'>Subscribe</button>
+                </div>
+            </div>
         </div>
     );
 };
