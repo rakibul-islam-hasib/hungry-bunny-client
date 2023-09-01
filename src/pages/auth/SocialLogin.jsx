f4284a407e0c9bb13b7ebd07b63c995896420502
Hasib Hossain <boraborhasib@gmail.com>
2023-09-01 11:14:37 +0600

    Google login updated

--- src/pages/auth/SocialLogin.jsx
+++ src/pages/auth/SocialLogin.jsx
@@ -5,8 +5,8 @@ import { handleFacebookRedirect, loginWithFacebook, loginWithGoogle } from '../.
 import { useEffect } from 'react';
 const SocialLogin = () => {
     const dispatch = useDispatch();
-    
-    
+
+
     useEffect(() => {
         dispatch(handleFacebookRedirect())
     }, [dispatch])
@@ -28,29 +28,34 @@ const SocialLogin = () => {
 
     return (
         <div className='pt-6'>
+
             <div className="text-center">
                 <div className="divider">OR SIGN IN WITH</div>
-                <div className='flex gap-4 pt-3 justify-center'>
+                <div className='flex pt-3 justify-center'>
                     <button
                         onClick={handleFacebook}
                         title="Facebook"
 
-                        className=' px-4 flex items-center py-2 gap-3 rounded-xl border-2 border-primary'
+                        className=' relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800'
                     >
-                        <FaFacebook className='text-2xl'></FaFacebook>
 
-                        Facebook
+                        <span className='flex gap-3 items-center relative px-10 py-2 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0'>
+                            <FaFacebook className='text-2xl'></FaFacebook>
+
+                            Facebook
+                        </span>
                     </button>
-
                     <button
                         onClick={googleLogin}
-                        title="Facebook"
-
-                        className=' px-3 flex items-center py-2 gap-3 rounded-xl border-2 border-primary'
+                        title="Google"
+                        className=' relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800'
                     >
-                        <FaGoogle className='text-2xl'></FaGoogle>
 
-                        Google
+                        <span className='flex gap-3 items-center relative px-10 py-2 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0'>
+                            <FaGoogle className='text-2xl'></FaGoogle>
+
+                            Google
+                        </span>
                     </button>
                 </div>
             </div>
