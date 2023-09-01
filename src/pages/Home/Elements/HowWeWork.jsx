1a9b0430a224af895767254f35952afab546413b
Hasib Hossain <boraborhasib@gmail.com>
2023-09-01 13:34:51 +0600

    All conflicts fixed and remove the stack card from our project

---
+++ src/pages/Home/Elements/HowWeWork.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import illustration from '../../../assets/img/work/illustration-1.png';
+import illustration2 from '../../../assets/img/work/illustration-2.png';
+import illustration3 from '../../../assets/img/work/illustration-3.png';
+
+const HowWeWork = () => {
+    return (
+        <div>
+            <h1>How We Work</h1>
+        </div>
+    );
+};
+
+export default HowWeWork;
