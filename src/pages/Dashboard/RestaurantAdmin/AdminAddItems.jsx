94dc8e718c456e5d3cdb07949b427053d7962d93
Hasib Hossain <boraborhasib@gmail.com>
2023-09-04 17:59:19 +0600

    Updated

--- src/pages/Dashboard/RestaurantAdmin/AdminAddItems.jsx
+++ src/pages/Dashboard/RestaurantAdmin/AdminAddItems.jsx
@@ -1,32 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 const AdminAddItems = () => {
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [imagePreview, setImagePreview] = useState(null);
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    setSelectedFile(file);
+    const selectedUrl = URL.createObjectURL(file);
+    setImagePreview(selectedUrl);
+  };
+
+  const handleFromSubmit = event => { 
+    event.preventDefault();
+    const formData = new FormData();
+    formData.append("image", selectedFile);
+    const data = Object.fromEntries(formData);
+    console.log(data);
+  }
+
+
   return (
     <div className="bg-white border-4 rounded-lg shadow relative m-10">
       <div className="flex items-start justify-between p-5 border-b rounded-t">
         <h3 className="text-xl font-semibold">Edit product</h3>
-        <button
-          type="button"
-          className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center"
-          data-modal-toggle="product-modal"
-        >
-          <svg
-            className="w-5 h-5"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            {/* <path
-              fill-rule="evenodd"
-              d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-              clip-rule="evenodd"
-            ></path> */}
-          </svg>
-        </button>
+        
       </div>
 
       <div className="p-6 space-y-6">
-        <form action="#">
+        <form onSubmit={e => handleFromSubmit(e)}>
           <div className="grid grid-cols-6 gap-6">
             <div className="col-span-6 sm:col-span-3">
               <label
@@ -69,10 +70,8 @@ const AdminAddItems = () => {
               </label>
               <input
                 type="number"
-                name="
-                Quantity"
-                id="
-                Quantity"
+                name="Quantity"
+                id="Quantity"
                 className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-cyan-600 focus:border-cyan-600 block w-full p-2.5"
                 placeholder="01"
                 required=""
@@ -108,6 +107,42 @@ const AdminAddItems = () => {
                 placeholder="Details"
               ></textarea>
             </div>
+            <div className="col-span-full">
+              <label
+                name="product-image"
+                className="text-sm font-medium text-gray-900 block mb-2"
+              >
+                Product Image
+              </label>
+              <div className="flex items-center space-x-2">
+                <label
+                  htmlFor="product-image"
+                  className="cursor-pointer text-white bg-primary hover:bg-green-700 focus:ring-4 focus:ring-cyan-200 font-medium rounded-full text-sm px-5 py-2.5 text-center"
+                >
+                  Upload Image
+                </label>
+                <span className="text-gray-500">{selectedFile ? selectedFile.name : "No file selected"}</span>
+                <input
+                  type="file"
+                  id="product-image"
+                  className="hidden"
+                  accept="image/*"
+                  onChange={handleFileChange}
+                />
+              </div>
+            </div>
+
+            {/* Image preview box */}
+            {imagePreview && (
+              <div className="col-span-full">
+                <label className="text-sm font-medium text-gray-900 block mb-2">
+                  Image Preview
+                </label>
+                <div className="max-w-xs">
+                  <img src={imagePreview} alt="Product Preview" className="w-full h-auto" />
+                </div>
+              </div>
+            )}
           </div>
         </form>
       </div>
