488923a693342b4150c87d2b924be235205adad5
tufaelkhan <tufaelkhan247@gmail.com>
2023-09-04 11:15:24 -0700

    add new style

--- src/pages/about/Subscribe.jsx
+++ src/pages/about/Subscribe.jsx
@@ -2,15 +2,15 @@ import { FaEnvelope } from "react-icons/fa";
 
 function Subscribe() {
     return (
-        <div className="mt-20 flex mb-20">
+        <div className="mt-20 flex mb-20 dark:text-white">
             <div className="md:w-1/2">
                 <img src="https://bslthemes.com/html/quickeat/assets/img/illustration-4.png" alt="" />
             </div>
             <div className="md:w-1/2 font-bold pt-7 ml-10 pr-6 pb-5">
                 <h1 className="text-5xl ">Get the menu of your favorite restaurants every day</h1>
-        <div className="flex">
-        <input className="mt-10 text-2xl p-3 rounded-2xl" type="email" placeholder="Enter your email" />
-        <button className="drop-shadow-lg uppercase hover:transition hover:duration-400 bg-orange-500 border-2 border-orange-500 hover:text-orange-500 hover:bg-white mt-8 text-white font-bold py-2 px-4 rounded-full flex items-center">
+        <div className="flex mt-10">
+        <input className="text-2xl dark:text-black p-3 rounded-2xl" type="email" placeholder="Enter your email" />
+        <button className="drop-shadow-lg uppercase hover:transition hover:duration-400 bg-orange-500 border-2 border-orange-500 hover:text-orange-500 hover:bg-white text-white font-bold rounded-2xl flex items-center text-2xl ml-3 pr-3 pl-3">
             Subscribe
           </button>
         </div>
