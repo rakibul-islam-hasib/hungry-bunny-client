38a0e9d7077ee5b411df05e73d51248773329852
Hasib Hossain <boraborhasib@gmail.com>
2023-09-08 16:55:29 +0600

    Dashboard navbar component added

---
+++ src/components/header/dashboard/DNavBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DNavBar = () => {
+    return (
+        <div className='md:hidden' >
+            <h1>This is navbar</h1>
+        </div>
+    );
+};
+
+export default DNavBar;
