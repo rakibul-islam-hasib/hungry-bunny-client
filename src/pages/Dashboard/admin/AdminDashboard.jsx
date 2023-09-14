9ecd2398a8483e4ef73ce511d226e524a80b69ac
Project_3 <anik2115@gmail.com>
2023-09-14 15:23:19 +0600

    card modified and responsive done

--- src/pages/Dashboard/admin/AdminDashboard.jsx
+++ src/pages/Dashboard/admin/AdminDashboard.jsx
@@ -15,30 +15,30 @@ const AdminDashboard = () => {
           <AdminDashboardChart/>
           {/* <AdminDashboardPieChart/> */}
         
-          <div className="grid grid-cols-4 gap-4 mt-20">
+          <div className="grid lg:grid-cols-4 gap-6 mt-20">
             <div className='px-4 py-10 bg-orange-200 rounded-lg'>
                 <h3 className='text-center text-2xl font-semibold'>Total Restaurant</h3>
-                <h1 className='text-center text-4xl font-semibold'>48</h1>
+                <h1 className='text-center text-6xl font-semibold'>48</h1>
             </div>
          
             <div className='px-4 py-10 bg-orange-200 rounded-lg'>
                 <h3 className='text-center text-2xl font-semibold'>Total Employess</h3>
-                <h1 className='text-center text-4xl font-semibold'>200</h1>
+                <h1 className='text-center text-6xl font-semibold'>200</h1>
             </div>
          
             <div className='px-4 py-10 bg-orange-200 rounded-lg'>
                 <h3 className='text-center text-2xl font-semibold'>Total Users</h3>
-                <h1 className='text-center text-4xl font-semibold'>1148</h1>
+                <h1 className='text-center text-6xl font-semibold'>1148</h1>
             </div>
          
             <div className='px-4 py-10 bg-orange-200 rounded-lg'>
                 <h3 className='text-center text-2xl font-semibold'>Total Menus</h3>
-                <h1 className='text-center text-4xl font-semibold'>536</h1>
+                <h1 className='text-center text-6xl font-semibold'>536</h1>
             </div>
          
           </div>
 
-          <div className="grid grid-cols-3">
+          <div className="grid lg:grid-cols-3">
             <AdmonDashboardProgress/>
             <AdminDashboardPieChart/>
             <AdminDashBoardList/>
