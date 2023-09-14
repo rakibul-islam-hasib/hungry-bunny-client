7e9d1e8d6c7ef65091722e5ec76a4ae4a5026340
Project_3 <anik2115@gmail.com>
2023-09-14 15:01:26 +0600

    chart added in dashboard

--- src/pages/Dashboard/admin/AdminDashboard.jsx
+++ src/pages/Dashboard/admin/AdminDashboard.jsx
@@ -11,10 +11,10 @@ const AdminDashboard = () => {
     
     return (
         <div className='mt-10'>
-          <h3 className='text-center text-4xl font-bold py-5 '>Customers States</h3>
+          <h3 className='text-center text-4xl font-bold py-5 '>Restaurant States</h3>
           <AdminDashboardChart/>
           {/* <AdminDashboardPieChart/> */}
-          <h3 className='text-center text-4xl font-bold py-5 mt-10'>Restaurant States</h3>
+        
           <div className="grid grid-cols-4 gap-4 mt-20">
             <div className='px-4 py-10 bg-orange-200 rounded-lg'>
                 <h3 className='text-center text-2xl font-semibold'>Total Restaurant</h3>
@@ -38,8 +38,9 @@ const AdminDashboard = () => {
          
           </div>
 
-          <div className="grid grid-cols-2">
+          <div className="grid grid-cols-3">
             <AdmonDashboardProgress/>
+            <AdminDashboardPieChart/>
             <AdminDashBoardList/>
           </div>
    
