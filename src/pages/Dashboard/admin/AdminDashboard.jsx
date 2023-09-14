a51f9916734f387445d3ba41aaf29cfdd56910d8
tufaelkhan <tufaelkhan247@gmail.com>
2023-09-13 18:39:34 -0700

    add style

--- src/pages/Dashboard/admin/AdminDashboard.jsx
+++ src/pages/Dashboard/admin/AdminDashboard.jsx
@@ -46,6 +46,33 @@ const AdminDashboard = () => {
     </form>
                 </div>
             <div className='flex items-center'>
+            <button
+      type="button"
+      className="relative mr-5 inline-flex items-center p-3 text-sm font-medium text-center text-white bg-orange-500 rounded-lg hover:bg-orange-500 "
+    >
+      <svg
+        className="w-5 h-5"
+        aria-hidden="true"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="currentColor"
+        viewBox="0 0 20 16"
+      >
+        <path d="m10.036 8.278 9.258-7.79A1.979 1.979 0 0 0 18 0H2A1.987 1.987 0 0 0 .641.541l9.395 7.737Z" />
+        <path d="M11.241 9.817c-.36.275-.801.425-1.255.427-.428 0-.845-.138-1.187-.395L0 2.6V14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2.5l-8.759 7.317Z" />
+      </svg>
+      <span className="sr-only">Notifications</span>
+      <div className="absolute inline-flex items-center justify-center w-6 h-6 text-xs font-bold text-white bg-red-500 border-2 border-white rounded-full -top-2 -right-2 dark:border-gray-900">20</div>
+    </button>
+            <button
+      type="button"
+      className="relative mr-5 h-11 inline-flex items-center p-3 text-sm font-medium text-center text-white bg-orange-500 rounded-lg hover:bg-orange-500 "
+    >
+      <svg className="w-6 h-6 text-white dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+    <path d="M15.133 10.632v-1.8a5.407 5.407 0 0 0-4.154-5.262.955.955 0 0 0 .021-.106V1.1a1 1 0 0 0-2 0v2.364a.944.944 0 0 0 .021.106 5.406 5.406 0 0 0-4.154 5.262v1.8C4.867 13.018 3 13.614 3 14.807 3 15.4 3 16 3.538 16h12.924C17 16 17 15.4 17 14.807c0-1.193-1.867-1.789-1.867-4.175Zm-13.267-.8a1 1 0 0 1-1-1 9.424 9.424 0 0 1 2.517-6.39A1.001 1.001 0 1 1 4.854 3.8a7.431 7.431 0 0 0-1.988 5.037 1 1 0 0 1-1 .995Zm16.268 0a1 1 0 0 1-1-1A7.431 7.431 0 0 0 15.146 3.8a1 1 0 0 1 1.471-1.354 9.425 9.425 0 0 1 2.517 6.391 1 1 0 0 1-1 .995ZM6.823 17a3.453 3.453 0 0 0 6.354 0H6.823Z"></path>
+</svg>
+      <span className="sr-only">Notifications</span>
+      <div className="absolute inline-flex items-center justify-center w-6 h-6 text-xs font-bold text-white bg-red-500 border-2 border-white rounded-full -top-2 -right-2 dark:border-gray-900">10</div>
+    </button>
             <img className='w-20 rounded-full' src="https://images.generated.photos/P2tzhpRdA_aiFi3kf2sPjw-vYJXi7hQE349GFBGi2ds/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/OTY4MjM4LmpwZw.jpg" alt="user image" />
         <div className='ml-5'>
         <h1 className='text-2xl font-bold text-gray-900'>Welcome  {firebaseUser?.displayName} </h1>
