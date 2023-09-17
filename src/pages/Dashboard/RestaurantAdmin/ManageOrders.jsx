883e14d2ed320797b0d3b71c280b8fb2cfba1365
Hasib Hossain <boraborhasib@gmail.com>
2023-09-17 23:13:12 +0600

    Manage order page work start done

---
+++ src/pages/Dashboard/RestaurantAdmin/ManageOrders.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ManageOrders = () => {
+    return (
+        <div>
+            <h2>Manage Orders</h2>
+        </div>
+    );
+};
+
+export default ManageOrders;
