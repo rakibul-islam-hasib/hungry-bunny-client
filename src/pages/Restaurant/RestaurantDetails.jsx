780ea07851e238b17c49419f9c27684fe33ebbef
tufaelkhan <tufaelkhan247@gmail.com>
2023-09-22 08:55:30 -0700

    add search bar

--- src/pages/Restaurant/RestaurantDetails.jsx
+++ src/pages/Restaurant/RestaurantDetails.jsx
@@ -1,6 +1,6 @@
 import { Rating } from '@smastrom/react-rating';
 import React, { useEffect, useState } from 'react';
-import { FaArrowRight, FaMailBulk, FaPlusSquare, FaShoppingCart, FaUserAstronaut } from 'react-icons/fa';
+import { FaShoppingCart} from 'react-icons/fa';
 import { BsStopwatch } from 'react-icons/bs';
 import { MdOutlineLocalOffer, MdDirectionsBike } from 'react-icons/md';
 import { PiBowlFood } from 'react-icons/pi';
@@ -74,7 +74,46 @@ function RestaurantDetails() {
     <p className='mt-4 mb-4'>Explore a world of diverse flavors. Our menu boasts a variety of cuisines, promising an exciting dining adventure with every order.</p>
   </div>
 </div>
-     
+     <div className='flex items-center justify-between mb-12 mt-6'>
+      <h2 className='text-3xl mt-8 font-bold'>All Food Items</h2>
+     <form className='mt-10 w-[50%]'>
+      <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">
+        Search
+      </label>
+      <div className="relative">
+        <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+          <svg
+            className="w-4 h-4 text-gray-500 dark:text-gray-400"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 20 20"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+            />
+          </svg>
+        </div>
+        <input
+          type="search"
+          id="default-search"
+          className="block w-full p-3 pl-10 text-xl text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-orange-500 focus:border-orange-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-orange-500 dark:focus:border-orange-500"
+          placeholder="Search food item, category "
+          required
+        />
+        <button
+          type="submit"
+          className="text-white absolute right-2.5 bottom-2.5 bg-black duration-500 hover:bg-orange-500 focus:ring-4 focus:outline-none focus:ring-orange-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-orange-600 dark:hover:bg-orange-700 dark:focus:ring-orange-800"
+        >
+          Search
+        </button>
+      </div>
+    </form>
+     </div>
     {/* food restaurant card */}
 <div className="dark:text-slate-300">
 <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 md:gap-8 gap-3 mb-14">
